06136c3e2bdc9a433b80e2e5a4a6c7ac68c07fe2
didzl <delpiero1987@naver.com>
2021-02-23 15:11:48 +0900

    goroutine test

--- main.go
+++ main.go
@@ -3,45 +3,25 @@ package main
 import (
 	"errors"
 	"fmt"
-	"net/http"
+	"time"
 )
 
 var errRequestFail = errors.New("request fail")
 
 
 func main() {
-	var results = make(map[string]string)
-	urls := []string{
-		"https://bbs.ruliweb.com/best/humor",
-		"https://dvdprime.com/g2/bbs/board.php?bo_table=comm&wr_id=21199644",
-		"https://www.44bits.io/ko/post/easy-deploy-with-docker",
-		"https://gall.dcinside.com/mgallery/board/view/?id=war&no=1700019&page=1",
-		"https://naver.com",
-		"https://google.com",
-		"https://amazon.com",
+	c := make(chan string)
+	//just doing main func
+	people := [5]string{"nico", "han", "ham", "lee", "bae"}
+	for _, person := range people {
+		go isGood(person, c)
 	}
-	results["gello"] = "hello"
-	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "fail"
-		}
-		results[url] = result
-	}
-	for url, result := range results{
-		fmt.Println(url, result)
-
+	for i:= 0; i<len(people); i++ {
+		fmt.Println(<-c)
 	}
 }
 
-
-func hitURL(url string) error {
-	fmt.Println("checking: ", url)
-	res, err := http.Get(url)
-	if err != nil || res.StatusCode >=400{
-		fmt.Println(err, res.StatusCode)
- 		return errRequestFail
-	}
-	return nil
+func isGood(person string, c chan string) {
+	time.Sleep(time.Second *5)
+	c <- person + " is good"
 }
